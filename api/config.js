e36ed9afaeca178abcb0395a5c18f0773560e34b
RAVS <shubham110267@gmail.com>
2026-07-17 15:03:04 +0530

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,6 @@
+/******************************************************
+ * UCPP API Configuration
+ ******************************************************/
+
+const API_URL = 
+"https://script.google.com/macros/s/AKfycbxUjX0EjxsxBxd7qu3CMwZFIDuNxWYBHwwzfRhaYL6AbRbGTjKwLcz3wOXk_jln7shJdA/exec";
