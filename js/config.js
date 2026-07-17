1a8121d40e538c3339f909c9f4713a2eaa4b3ccd
RAVS <shubham110267@gmail.com>
2026-07-17 17:28:37 +0530

    Delete js/config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-/******************************************************
- * API Configuration
- ******************************************************/
-
-const API_URL =
-"https://script.google.com/macros/s/AKfycbxUjX0EjxsxBxd7qu3CMwZFIDuNxWYBHwwzfRhaYL6AbRbGTjKwLcz3wOXk_jln7shJdA/exec";
