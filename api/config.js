a65b41b0ddf210190865adc0bd0ad9ca9528a123
RAVS <shubham110267@gmail.com>
2026-07-17 16:54:45 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,13 @@
 /******************************************************
- * UCPP API Configuration
+ * UCPP - API Configuration
  ******************************************************/
 
-const API_URL = 
-"https://script.google.com/macros/s/AKfycbxUjX0EjxsxBxd7qu3CMwZFIDuNxWYBHwwzfRhaYL6AbRbGTjKwLcz3wOXk_jln7shJdA/exec";
+const CONFIG = {
+
+  API_URL: "https://script.google.com/macros/s/AKfycbxUjX0EjxsxBxd7qu3CMwZFIDuNxWYBHwwzfRhaYL6AbRbGTjKwLcz3wOXk_jln7shJdA/exec",
+
+  APP_NAME: "Udayan Care Placement Portal",
+
+  VERSION: "1.0.0"
+
+};
